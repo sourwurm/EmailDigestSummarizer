6ab93c4fc29f5418d2e146f6ccca9c9e24572281
David Lopez <38804883+sourwurm@users.noreply.github.com>
2020-08-24 16:01:34 -0700

    Update DD_script.py

--- DD_script.py
+++ DD_script.py
@@ -385,7 +385,7 @@ df['Published'] = df['Published'].apply(one_more)
 
 top = '''<!DOCTYPE html>
 <html lang="en">
-<title>Your Daily Digest</title>
+<title>Daily Digest</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
